c78147840497154947c52cfe776b186cc1a62592
CYBERCODERoss <cybertech75@proton.me>
2023-11-29 09:23:55 +0500

    jjd

--- day01/main.py
+++ day01/main.py
@@ -1,5 +1,5 @@
 print("Welcome To Band Name Generator")
-# Asking the user for the city that they grew up in.
+# As    king the user for the city that they grew up in.
 city_name = input("In which city did you grow up in : ")
 # Asking the user for the name of a pet.
 pet_name = input("Write the name of a pet  : ")
